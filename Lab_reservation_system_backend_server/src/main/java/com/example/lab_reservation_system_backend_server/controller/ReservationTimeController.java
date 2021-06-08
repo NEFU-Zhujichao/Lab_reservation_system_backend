270ea4c2ab0861c119aa4cdf0c34d7dacfa563f3
13949 <1394949540@qq.com>
2021-06-08 16:37:03 +0800

    添加导出预约信息功能

--- Lab_reservation_system_backend_server/src/main/java/com/example/lab_reservation_system_backend_server/controller/ReservationTimeController.java
+++ Lab_reservation_system_backend_server/src/main/java/com/example/lab_reservation_system_backend_server/controller/ReservationTimeController.java
@@ -1,5 +1,8 @@
 package com.example.lab_reservation_system_backend_server.controller;
 
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.entity.ExportParams;
+import cn.afterturn.easypoi.excel.entity.enmus.ExcelType;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.lab_reservation_system_backend_server.pojo.ReservationTime;
 import com.example.lab_reservation_system_backend_server.pojo.RespBean;
@@ -7,11 +10,18 @@ import com.example.lab_reservation_system_backend_server.pojo.RestPageBean;
 import com.example.lab_reservation_system_backend_server.service.IReservationTime;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/reservationTime")
@@ -30,5 +40,23 @@ public class ReservationTimeController {
         // 返回带有分页的对象列表
         Page<ReservationTime> timePage = reservationTime.page(page);
         return new RestPageBean(timePage.getTotal(),timePage.getRecords());
+    }
+
+    @ApiOperation(value = "导出预约信息")
+    @GetMapping(value = "/export",produces = "application/octet-stream")
+    public void exportReservationTime(HttpServletResponse response){
+        List<ReservationTime> list = reservationTime.list();
+        // 03版Excel兼容性更强
+        ExportParams params = new ExportParams("预约信息","预约信息", ExcelType.HSSF);
+        Workbook workbook = ExcelExportUtil.exportExcel(params, ReservationTime.class, list);
+        try(ServletOutputStream outputStream = response.getOutputStream()) {
+            // 流形式传输
+            response.setHeader("content-type","application/octet-stream");
+            // 防止中文乱码
+            response.setHeader("content-disposition","attachment;filename=" + URLEncoder.encode("预约信息.xls","UTF-8"));
+            workbook.write(outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Lab_reservation_system_backend_server/src/main/java/com/example/lab_reservation_system_backend_server/pojo/ReservationTime.java
+++ Lab_reservation_system_backend_server/src/main/java/com/example/lab_reservation_system_backend_server/pojo/ReservationTime.java
@@ -1,5 +1,6 @@
 package com.example.lab_reservation_system_backend_server.pojo;
 
+import cn.afterturn.easypoi.excel.annotation.Excel;
 import com.baomidou.mybatisplus.annotation.FieldStrategy;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
@@ -30,33 +31,43 @@ public class ReservationTime implements Serializable {
     private Long uid;
 
     @ApiModelProperty(value = "教师名字")
+    @Excel(name = "教师名字",width = 20)
     private String uname;
 
     @ApiModelProperty(value = "实验课程id")
     private Long cid;
 
     @ApiModelProperty(value = "实验课程名字")
+    @Excel(name = "实验课程名字",width = 20)
     private String cname;
 
     @ApiModelProperty(value = "实验室名字")
+    @Excel(name = "实验室名字",width = 20)
     private String labName;
 
     @ApiModelProperty(value = "实验周次")
+    @Excel(name = "实验周次",suffix = "周")
     private Integer week;
 
     @ApiModelProperty(value = "实验星期")
+    @Excel(name = "实验星期")
     private Integer day;
 
     @ApiModelProperty(value = "实验节数")
+    @Excel(name = "实验节数",suffix = "节")
     private Integer section;
 
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @TableField(updateStrategy = FieldStrategy.NEVER)
+    @ApiModelProperty(value = "创建日期")
+    @Excel(name = "创建日期",format = "yyyy-MM-dd",width = 20)
     private LocalDateTime createTime;
 
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @TableField(updateStrategy = FieldStrategy.NEVER)
+    @ApiModelProperty(value = "修改日期")
+    @Excel(name = "修改日期",format = "yyyy-MM-dd",width = 20)
     private LocalDateTime updateTime;
 }
